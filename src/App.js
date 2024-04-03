50f8e7d163fae436a4adc9ed47e3a7239955f186
Vishal Kumar <iamvishukr@gmail.com>
2024-04-03 22:36:12 +0530

    Creating Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
-
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className="top-panel"></div>
+      <div className="bottom-panel">
+        <iframe
+          title="output"
+          sandbox="allow-script"
+          width="100%"
+          height="100%"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Editor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+const Editor = () => {
+  return (
+    <div>Editor</div>
+  )
+}
+
+export default Editor
